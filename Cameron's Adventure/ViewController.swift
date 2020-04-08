cc9c0f6a5f2ecea9b0310098c5c6a78885edc053
Cameron Buckley <cbuckley@astate.edu>
2020-04-07 20:31:14 -0500

    Changed some things

--- Cameron's Adventure/ViewController.swift
+++ Cameron's Adventure/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 //Change
+    //make another change
+    
+    func blah(){
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
