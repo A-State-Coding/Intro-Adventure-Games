3ef1c243598b40d172abcb2136c56ef9c3be09d4
Jodie Kelso <jodie.kelso@smail.astate.edu>
2020-04-14 10:07:09 -0500

    Initial Commit

--- Wanderscape_adventure_game/ViewController.swift
+++ Wanderscape_adventure_game/ViewController.swift
@@ -14,7 +14,49 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func startButtonTapped(_ sender: Any) {
+    }
+    @IBAction func introContinueTapped(_ sender: Any) {
+    }
+    @IBAction func riverChoiceATapped(_ sender: Any) {
+    }
+    @IBAction func riverContiueTapped(_ sender: Any) {
+    }
+    @IBAction func introSwampContinueTapped(_ sender: Any) {
+    }
+  
+    @IBAction func riverChoiceBTapped(_ sender: Any) {
+    }
+    @IBAction func riverOKContinue(_ sender: Any) {
+    }
+    @IBAction func backToRiverContinue(_ sender: Any) {
+    }
+    @IBAction func swampChoiceBTapped(_ sender: Any) {
+    }
+    @IBAction func pastTreeContinueTapped(_ sender: Any) {
+    }
+    @IBAction func beatTheGatorTapped(_ sender: Any) {
+        //if gator isnt tapped within time, back to start?
+    }
+    @IBAction func swampChoiceATapped(_ sender: Any) {
+    }
+    @IBAction func swampSwimContinueTapped(_ sender: Any) {
+    }
+    @IBAction func swampMadeItContinueTapped(_ sender: Any) {
+    }
+    
+    @IBAction func swampOutroContinueTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func goHomeContinueTapped(_ sender: Any) {
+    }
+    @IBAction func endContinueTapped(_ sender: Any) {
+    }
 }
 
+
+
+
+
+//add sounds to river, swamp, & city.
