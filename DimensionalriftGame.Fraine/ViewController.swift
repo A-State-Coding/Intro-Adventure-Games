9ed37c10481f0b857867c21364bc1bafe1ad7bc2
kara "woodbine" fraine <woodbine@gmail.com>
2020-04-19 14:20:00 -0500

    Initial Commit

---
+++ DimensionalriftGame.Fraine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DimensionalriftGame.Fraine
+//
+//  Created by Kara Fraine on 4/19/20.
+//  Copyright © 2020 Kara Fraine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
