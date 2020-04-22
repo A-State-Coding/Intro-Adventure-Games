09e796ba103792fd1cd804b9f5c296315e953712
kara "woodbine" fraine <woodbine@gmail.com>
2020-04-22 14:06:13 -0500

    my project on 04/22/2020

---
+++ DimensionalriftGame.Fraine/viewcontrollers/controllers 6-10/user6ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  user6ViewController.swift
+//  DimensionalriftGame.Fraine
+//
+//  Created by Kara Fraine on 4/20/20.
+//  Copyright © 2020 Kara Fraine. All rights reserved.
+//
+
+import UIKit
+
+class user6ViewController: UIViewController {
+    @IBOutlet weak var gowwith: UIButton!
+    @IBOutlet weak var returnLabel: UILabel!
+    @IBOutlet weak var runaway: UIButton!
+    @IBAction func returnbutton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DimensionalriftGame.Fraine/viewcontrollers/controllers 6-10/user9ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  user9ViewController.swift
+//  DimensionalriftGame.Fraine
+//
+//  Created by Kara Fraine on 4/21/20.
+//  Copyright © 2020 Kara Fraine. All rights reserved.
+//
+
+import UIKit
+
+class user9ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
